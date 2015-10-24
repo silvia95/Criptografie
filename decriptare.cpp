80dcca835c1c28bfa8d9637b490475b83ecf84e9
Paul92 <paul.gafton@gmail.com>
2015-10-24 21:48:30 +0100

    Bla bla

--- decriptare.cpp
+++ decriptare.cpp
@@ -1,126 +1,280 @@
-#include<iostream>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NO_OF_LETTERS 26
+#define TEXT_LEN 10000
 
 using namespace std;
 
-FILE *f = fopen ("criptotext.txt", "r");
-FILE *g = fopen ("decryption.txt", "w");
+FILE *fin  = fopen ("criptotext.txt", "r");
+FILE *fout = fopen ("decryption.txt", "w");
 
-char litere[30], text[100000], litereThe[26], litereTo[26], key[26];
-int frecventaLitere[30], frecvThe[26], frecvTo[26], 
-    nrLitereCodificate = 0, nrLitereTo = 0, 
-    nrLitere = 0, nrLitereThe = 0;
+char litereThe[NO_OF_LETTERS];
+char litereTo[NO_OF_LETTERS];
+char key[NO_OF_LETTERS];
+
+
+int frecvTo[NO_OF_LETTERS]; 
+int frecvThe[NO_OF_LETTERS];
+
+int nrLitere           = 0;
+int nrLitereTo         = 0;
+int nrLitereThe        = 0;
+
+struct Letter {
+    char letter;
+    int frequency;
+};
+
+int comp(const void *a, const void *b)
+{
+    return (((struct Letter*)b)->frequency - ((struct Letter*)a)->frequency);
+}
 
 int main() {
-    char c;
-    while (! feof(f)) {
-        fscanf (f, "%c", &c);
-        text[nrLitereCodificate++] = c;
-    }
-    for (int i = 0; i < 30; i++) {
-        frecventaLitere[i] = 0;
+
+    struct Letter letters[NO_OF_LETTERS];
+
+    for (int i = 0; i < NO_OF_LETTERS; i++) {
+        letters[i].letter = i + 'A';
+        letters[i].frequency = 0;
     }
 
-    for (int i = 0; i < nrLitereCodificate; i++) {
-        frecventaLitere[text[i] - 65]++;
+    char newCharacter;
+    char text[TEXT_LEN];
+    int noOfLetters = 0;
+    while (fscanf(fin, "%c", &newCharacter) == 1) {
+        text[noOfLetters++] = newCharacter;
+        letters[newCharacter - 'A'].frequency++;
     }
 
-    for (int x = 'A'; x <= 'Z'; x++) {
-        litere[nrLitere++] = x;
+    qsort(letters, NO_OF_LETTERS, sizeof(struct Letter), comp);
+
+    char key[] = "ETAOINSRHLDCUMFPGWYBVKXJQZ";
+
+
+
+
+    char decryptedText[TEXT_LEN];
+    for (int count = 0; count < noOfLetters; count++) {
+        int letterIndex = 0;
+        if (text[count] == letters[0].letter)
+            decryptedText[count] = 'E';
+        else if (text[count] == letters[1].letter)
+            decryptedText[count] = 'T';
+        else if (text[count] == letters[2].letter)
+            decryptedText[count] = 'A';
+        else
+            decryptedText[count] = '*';
     }
 
-    for (int i = 0; i < 25; i++) {
-        for (int j = i+1; j < 26; j++) {
-            if (frecventaLitere[i] < frecventaLitere[j]) {
-                int aux;
-                char aux_a;
-                aux = frecventaLitere[i];
-                aux_a = litere[i];
-                frecventaLitere[i] = frecventaLitere[j];
-                litere[i] = litere[j];
-                frecventaLitere[j] = aux;
-                litere[j] = aux_a;
-            }
-        }
-    }
-    
-    for (int  i = 0; i < 26; i++) {
-        cout << "litera "<< litere[i] << " apare cu frecventa "
-            << frecventaLitere[i] << "\n";
-    }
-    for (int i = 0; i < nrLitereCodificate; i++) {
-        if (text[i] == litere[0]) {
-            fprintf(g, "E");
-            key[litere[0] - 65] = litere[0];
-            text[i] = 'E';
-        } else if (text[i] == litere[1]) {
-            fprintf(g, "T");
-            key[litere[1] - 65] = litere[1];
-            text[i] = 'T';
-        } else if (text[i] == litere[2]) {
-            fprintf(g, "A");
-            key[litere[2] - 65] = litere[2];
-            text[i] = 'A';
-        } else {
-            fprintf(g, "*");
-        }
-    }
-    for (int i = 0; i < 25; i++) {
-        if(text[i] == 'T' && text[i+2] == 'E') {
-            frecvThe[text[i+1] - 65]++;
-        }
-    }
-    for (int x = 'A'; x <= 'Z'; x++) {
-        litereThe[nrLitereThe++] = x;
-    }
 
-    for (int i = 0; i < 25; i++) {
-        for (int j = i+1; j < 26; j++) {
-            if (frecvThe[i] < frecvThe[j]) {
-                int aux1;
-                char aux_The;
-                aux1 = frecvThe[i];
-                aux_The = litereThe[i];
-                frecvThe[i] = frecvThe[j];
-                litereThe[i] = litereThe[j];
-                frecvThe[j] = aux1;
-                litereThe[j] = aux_The;
+
+
+    int theTriadCount[NO_OF_LETTERS];
+    for (int count = 0; count < NO_OF_LETTERS; count++)
+        theTriadCount[count] = 0;
+
+    int maxLetterId = 0;
+    for (int count = 0; count < noOfLetters - 2; count++) {
+        if (decryptedText[count] == 'T' && decryptedText[count + 2] == 'E') {
+            if (++theTriadCount[text[count + 1] - 'A'] > theTriadCount[maxLetterId]) {
+                maxLetterId = text[count + 1] - 'A';
             }
         }
     }
 
-    key['H'-65] = litereThe[0];
-
-    for (int i = 0; i < 25; i++) {
-        if (text[i] == 'T' && text[i] != key[frecvThe[0] - 65]) {
-            frecvTo[text[i+1] - 65]++;
+    for (int count = 0; count < noOfLetters; count++) {
+        if (text[count] == maxLetterId + 'A') {
+            decryptedText[count] = 'H';
         }
     }
-    for (int x = 'A'; x <= 'Z'; x++) {
-        litereTo[nrLitereTo++] = x;
-    }
 
-    for (int i = 0; i < 25; i++) {
-        for ( int j = i+1; j < 26; j++) {
-            if (frecvTo[i] < frecvTo[j]) {
-                int aux2;
-                char aux_To;
-                aux2 = frecvTo[i];
-                aux_To = litereTo[i];
-                frecvTo[i] = frecvTo[j];
-                litereTo[i] = litereTo[j];
-                frecvTo[j] = aux2;
-                litereTo[j] = aux_To;
+
+
+
+    int theDiadCount[NO_OF_LETTERS];
+    for (int count = 0; count < NO_OF_LETTERS; count++)
+        theDiadCount[count] = 0;
+
+    maxLetterId = 0;
+    for (int count = 0; count < noOfLetters - 1; count++) {
+        if (decryptedText[count] == 'T' && decryptedText[count + 1] == '*') {
+            if (++theDiadCount[text[count + 1] - 'A'] > theDiadCount[maxLetterId]) {
+                maxLetterId = text[count + 1] - 'A';
             }
         }
     }
 
-    key['O' - 65] = litereTo[0];
-
-    cout << "\n";
-    for (int i = 0; i < 26; i++) {
-        cout << "pe pozitia "<< i << " cheia este " << key[i] << "\n";
+    for (int count = 0; count < noOfLetters; count++) {
+        if (text[count] == maxLetterId + 'A') {
+            decryptedText[count] = 'O';
+        }
     }
+
+
+
+
+    for (int count = 0; count < NO_OF_LETTERS; count++)
+        theDiadCount[count] = 0;
+
+    maxLetterId = 0;
+    for (int count = 0; count < noOfLetters - 1; count++) {
+        if (decryptedText[count] == 'E' && decryptedText[count + 1] == '*') {
+            if (++theDiadCount[text[count + 1] - 'A'] > theDiadCount[maxLetterId]) {
+                maxLetterId = text[count + 1] - 'A';
+            }
+        }
+    }
+
+    for (int count = 0; count < noOfLetters; count++) {
+        if (text[count] == maxLetterId + 'A') {
+            decryptedText[count] = 'R';
+        }
+    }
+
+
+
+
+    for (int count = 0; count < NO_OF_LETTERS; count++)
+        theDiadCount[count] = 0;
+
+    maxLetterId = 0;
+    for (int count = 0; count < noOfLetters - 1; count++) {
+        if (decryptedText[count] == 'A' && decryptedText[count + 1] == '*') {
+            if (++theDiadCount[text[count + 1] - 'A'] > theDiadCount[maxLetterId]) {
+                maxLetterId = text[count + 1] - 'A';
+            }
+        }
+    }
+
+    for (int count = 0; count < noOfLetters; count++) {
+        if (text[count] == maxLetterId + 'A') {
+            decryptedText[count] = 'N';
+        }
+    }
+
+
+
+
+    for (int count = 0; count < NO_OF_LETTERS; count++)
+        theTriadCount[count] = 0;
+
+    for (int count = 0; count < noOfLetters - 2; count++) {
+        if (decryptedText[count] == 'A' && decryptedText[count + 1] == 'N' && decryptedText[count + 2] == '*') {
+            if (++theTriadCount[text[count + 2] - 'A'] > theTriadCount[maxLetterId]) {
+                maxLetterId = text[count + 1] - 'A';
+            }
+        }
+    }
+
+    for (int count = 0; count < noOfLetters; count++) {
+        if (text[count] == maxLetterId + 'A') {
+            decryptedText[count] = 'D';
+        }
+    }
+
+
+
+
+//    struct Letter doubleLetters[NO_OF_LETTERS];
+//
+//    for (int i = 0; i < NO_OF_LETTERS; i++) {
+//        doubleLetters[i].letter = i + 'A';
+//        doubleLetters[i].frequency = 0;
+//    }
+//
+//    for (int count = 0; count < noOfLetters - 1; count++) {
+//        if (text[count] == text[count + 1] && decryptedText[count] == '*') {
+//            doubleLetters[text[count] - 'A'].frequency++;
+//        }
+//    }
+//
+//    qsort(doubleLetters, NO_OF_LETTERS, sizeof(struct Letter), comp);
+//
+//    for (int i = 0; i < NO_OF_LETTERS; i++) {
+//        printf("%c %d\n", doubleLetters[i].letter, doubleLetters[i].frequency);
+//    }
+//
+//    for (int count = 0; count < noOfLetters; count++) {
+//        int letterIndex = 0;
+//        if (text[count] == doubleLetters[0].letter)
+//            decryptedText[count] = 'S';
+//        //else if (text[count] == doubleLetters[1].letter)
+//        //    decryptedText[count] = 'F';
+//        //else if (text[count] == doubleLetters[2].letter)
+//        //    decryptedText[count] = 'L';
+//        //else if (text[count] == doubleLetters[3].letter)
+//        //    decryptedText[count] = 'M';
+//    }
+//
+//
+
+    for (int count = 0; count < noOfLetters; count++) {
+        printf("%c", decryptedText[count]);
+    }
+
+
+
+
+
+//    for (int i = 0; i < 25; i++) {
+//        if(text[i] == 'T' && text[i+2] == 'E') {
+//            frecvThe[text[i+1] - 65]++;
+//        }
+//    }
+//    for (int x = 'A'; x <= 'Z'; x++) {
+//        litereThe[nrLitereThe++] = x;
+//    }
+//
+//    for (int i = 0; i < 25; i++) {
+//        for (int j = i+1; j < 26; j++) {
+//            if (frecvThe[i] < frecvThe[j]) {
+//                int aux1;
+//                char aux_The;
+//                aux1 = frecvThe[i];
+//                aux_The = litereThe[i];
+//                frecvThe[i] = frecvThe[j];
+//                litereThe[i] = litereThe[j];
+//                frecvThe[j] = aux1;
+//                litereThe[j] = aux_The;
+//            }
+//        }
+//    }
+//
+//    key['H'-65] = litereThe[0];
+//
+//    for (int i = 0; i < 25; i++) {
+//        if (text[i] == 'T' && text[i] != key[frecvThe[0] - 65]) {
+//            frecvTo[text[i+1] - 65]++;
+//        }
+//    }
+//    for (int x = 'A'; x <= 'Z'; x++) {
+//        litereTo[nrLitereTo++] = x;
+//    }
+//
+//    for (int i = 0; i < 25; i++) {
+//        for ( int j = i+1; j < 26; j++) {
+//            if (frecvTo[i] < frecvTo[j]) {
+//                int aux2;
+//                char aux_To;
+//                aux2 = frecvTo[i];
+//                aux_To = litereTo[i];
+//                frecvTo[i] = frecvTo[j];
+//                litereTo[i] = litereTo[j];
+//                frecvTo[j] = aux2;
+//                litereTo[j] = aux_To;
+//            }
+//        }
+//    }
+//
+//    key['O' - 65] = litereTo[0];
+//
+//    cout << "\n";
+//    for (int i = 0; i < 26; i++) {
+//        cout << "pe pozitia "<< i << " cheia este " << key[i] << "\n";
+//    }
 }
 
 
